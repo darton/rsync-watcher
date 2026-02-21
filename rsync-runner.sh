07f4ee120428c529ec50ecfb3b3b3d446818c4a9
Dariusz Kowalczyk <darton@users.noreply.github.com>
2026-02-21 17:05:26 +0100

    Implement file type checks in rsync-runner.sh
    Add file type checks to skip syncing certain files.

--- rsync-runner.sh
+++ rsync-runner.sh
@@ -7,6 +7,12 @@ REMOTE_HOST="127.0.0.1"
 REMOTE_DIR="~/monitory"
 REMOTE_PORT=2211
 
-# 
+case "$FILE" in
+    *.tmp|*.part|*.swp|*.swx|*.~lock.*|*.ht|*.env|*.partial
+        exit 0
+        ;;
+esac
+
+ 
 #echo "$(date '+%Y-%m-%d %H:%M:%S') Synchronizacja katalogu upload" >> "$HOME/rsync-runner.log"
 rsync -avz -e "ssh -p $REMOTE_PORT" "$FILE" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}/"
