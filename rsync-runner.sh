56a4b01ef661c1dcab85c8b81cde8c5834e2316e
Dariusz Kowalczyk <darton@users.noreply.github.com>
2026-02-21 16:52:21 +0100

    Remove logging from rsync-runner.sh
    Removed optional logging for synchronization.

--- rsync-runner.sh
+++ rsync-runner.sh
@@ -7,8 +7,6 @@ REMOTE_HOST="127.0.0.1"
 REMOTE_DIR="~/monitory"
 REMOTE_PORT=2211
 
-# Logowanie (opcjonalne)
-echo "$(date '+%Y-%m-%d %H:%M:%S') Synchronizacja katalogu upload" >> "$HOME/rsync-runner.log"
-
-# Wysyłka rsync
+# 
+#echo "$(date '+%Y-%m-%d %H:%M:%S') Synchronizacja katalogu upload" >> "$HOME/rsync-runner.log"
 rsync -avz --delete -e "ssh -p $REMOTE_PORT" "$LOCAL_DIR" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}"
